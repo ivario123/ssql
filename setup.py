04edbefef48a3353865640f945e354fa7e713db5
ivario123 <ivar.jonsson18@gmail.com>
2023-01-20 10:22:18 +0100

    added dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name="ssql",
     version="0.0.1",
     author_email="ivajns-9@student.ltu.se",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "sshtunnel",
+        "mysql",
+        "mysql-connector-python"
+    ],
 )
